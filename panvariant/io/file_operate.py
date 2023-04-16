7793450280fac29edecbde4ed2b28545d086efc9
sc-zhang <zsc-zhang@foxmail.com>
2023-04-17 00:35:48 +0800

    add variant loader

--- panvariant/io/file_operate.py
+++ panvariant/io/file_operate.py
@@ -30,3 +30,27 @@ class FastaIO:
                 else:
                     seq += line.strip().upper()
             self.fasta_db[gid] = seq
+
+
+class VariantIO:
+    def __init__(self, variant_file):
+        self.__variant_file = variant_file
+        self.samples = None
+        # self.variants is 2-D list, like:
+        # [[pos, ref, alt(dict), [0, 1, 1, 0, ...](sample genotype)
+        # ...
+        # ]
+        self.variants = []
+
+    def read_var(self):
+        with open(self.__variant_file, 'r') as fin:
+            for line in fin:
+                data = line.strip().split()
+                if line[0] == '#':
+                    self.samples = data[3:]
+                else:
+                    pos = int(data[0])
+                    ref = data[1]
+                    alt_sites = data[2].split(',')
+                    alt = {_+1: alt_sites[_] for _ in range(len(alt_sites))}
+                    self.variants.append([pos, ref, alt, data[3:]])
---
+++ panvariant/workflow/variant_classifier.py
@@ -0,0 +1,8 @@
+from panvariant.io.file_operate import VariantIO
+from panvariant.io.message import Message as Msg
+from pathos.multiprocessing import Pool
+from os import listdir, path
+
+
+def __variant_caller_for_single_file(var_file, classified_file):
+    pass

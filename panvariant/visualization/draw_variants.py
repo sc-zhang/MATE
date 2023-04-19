8cceaab1ffe78bc040b3544dfbf1ae1b1a38d8f4
sc-zhang <zsc-zhang@foxmail.com>
2023-04-19 13:02:09 +0800

    adjust picture

--- panvariant/visualization/draw_variants.py
+++ panvariant/visualization/draw_variants.py
@@ -12,21 +12,22 @@ mpl.use('Agg')
 def __draw_genes_in_association(ref_cds, asc_file, pic_dir):
     if pic_dir and not path.exists(pic_dir):
         makedirs(pic_dir)
-    fast_io = FastaIO(ref_cds)
-    fast_io.read_fasta()
-    ref_cds_len_db = fast_io.seq_len_db
+    fasta_io = FastaIO(ref_cds)
+    fasta_io.read_fasta()
+    ref_cds_len_db = fasta_io.seq_len_db
     var_sites_db = AssociateIO.read_asc(asc_file)
 
-    variant_color_db = {'Missing': 'black',
-                        '-': 'crimson',
+    variant_color_db = {'-': 'crimson',
                         'A': 'darkgreen',
                         'T': 'deeppink',
                         'C': 'darkorange',
                         'G': 'gold'}
     for gene in var_sites_db:
         Msg.info("\tPlotting %s in %s" % (gene, pic_dir))
-        plt.figure(figsize=(20, 8), dpi=300)
+        plt.figure(figsize=(20, 2*len(var_sites_db[gene]['geno'])), dpi=300)
         best_cnt = {}
+        min_pos = fasta_io.seq_len_db[gene]
+        max_pos = 0
         for sample_idx in range(len(var_sites_db[gene]['geno'])):
             best_cnt[sample_idx] = 0
             plt.plot([0, ref_cds_len_db[gene]], [sample_idx, sample_idx], color='grey', linewidth=1)
@@ -37,44 +38,69 @@ def __draw_genes_in_association(ref_cds, asc_file, pic_dir):
                 alt = var_sites_db[gene]['alt'][geno_idx]
                 best_geno = var_sites_db[gene]['best_geno'][geno_idx]
 
+                if pos > max_pos:
+                    max_pos = pos
+                if pos < min_pos:
+                    min_pos = pos
+
                 if geno == best_geno:
                     best_cnt[sample_idx] += 1
 
                 if geno == '-':
-                    plt.scatter(pos, sample_idx, s=50, marker='x', color='black')
+                    Msg.warn("\tUnexpected variant found")
                 elif geno == '0':
                     for site in range(len(ref)):
                         ref_geno = ref[site]
-                        plt.scatter(pos+site, sample_idx, s=50,
-                                    marker="p" if ref_geno == '-' else 's',
+                        plt.scatter(pos+site, sample_idx, s=100,
+                                    marker="X" if ref_geno == '-' else 's',
                                     color=variant_color_db[ref_geno],
+                                    alpha=0.75,
                                     zorder=99)
                 else:
                     for site in range(len(alt[int(geno)-1])):
                         alt_geno = alt[int(geno)-1][site]
-                        plt.scatter(pos + site, sample_idx, s=50,
-                                    marker="p" if alt_geno == '-' else 's',
+                        plt.scatter(pos + site, sample_idx, s=100,
+                                    marker="X" if alt_geno == '-' else 's',
                                     color=variant_color_db[alt_geno],
+                                    alpha=0.75,
                                     zorder=99)
 
         for variant_type in variant_color_db:
-            plt.scatter(0, -1, s=50, marker="p" if variant_type == '-' else 's',
+            plt.scatter(0, -1, s=100, marker="X" if variant_type == '-' else 's',
                         color=variant_color_db[variant_type],
-                        label=variant_type)
+                        label=variant_type if variant_type != '-' else "Deletion")
         plt.legend(bbox_to_anchor=(1.01, 0.5), loc="center left", fontsize=20, frameon=False)
-        plt.ylim(-.5, len(var_sites_db[gene]['geno'])-.5)
+
+        plt.ylim(-.1, len(var_sites_db[gene]['geno'])-.9)
+
+        pos_range_length = max_pos - min_pos
+        if pos_range_length == 0:
+            left = max_pos-fasta_io.seq_len_db[gene]*.2
+            right = max_pos+fasta_io.seq_len_db[gene]*.2
+        else:
+            left = min_pos-pos_range_length*.2
+            right = max_pos+pos_range_length*.2
+
+        plt.xlim(left, right)
+        # avoid range show in label out of gene range
+        left = max(0, left)
+        right = min(right, fasta_io.seq_len_db[gene])
+        plt.xticks([])
+        plt.xlabel("%s (%d~%d:%d)" % (gene, int(left)+1, int(right), fasta_io.seq_len_db[gene]), fontsize=20)
         best_idx = 0
         best_value = 0
         for sample_idx in best_cnt:
             if best_cnt[sample_idx] > best_value:
                 best_value = best_cnt[sample_idx]
                 best_idx = sample_idx
-        plt.text(-25, best_idx, "Best Type", va='center', ha='right', fontsize=15)
-        plt.title("Alleles of gene %s" % gene, fontsize=25)
-        plt.axis('off')
+        plt.yticks([best_idx], ["Best"], fontsize=20)
+        ax = plt.gca()
+        ax.tick_params("y", length=0)
+        ax.spines['top'].set_visible(False)
+        ax.spines['bottom'].set_visible(False)
+        ax.spines['left'].set_visible(False)
+        ax.spines['right'].set_visible(False)
         pic_file = path.join(pic_dir, "%s.pdf" % gene)
-        plt.savefig(pic_file, bbox_inches='tight')
-        pic_file = path.join(pic_dir, "%s.png" % gene)
         plt.savefig(pic_file, bbox_inches='tight')
         plt.close('all')
     Msg.info("\tFinished")
